e15143d5ec0dc8ff12381d17e9392b5a9b7a73a9
ccrolliston@gmail.com <ccrolliston@gmail.com@f29953f8-ae01-ac56-c041-7d902c977e6c>
2012-08-19 14:34:44 +0000

    Amended the copyright section for the file just fixed

--- trunk/VCL Demos/ResaveTest/ResaveCompleteDlg.pas
+++ trunk/VCL Demos/ResaveTest/ResaveCompleteDlg.pas
@@ -1,7 +1,7 @@
 {**************************************************************************************}
 {                                                                                      }
 { CCR Exif - Delphi class library for reading and writing Exif metadata in JPEG files  }
-{ Version 1.5.1                                                                        }
+{ Version 1.5.2 beta                                                                   }
 {                                                                                      }
 { The contents of this file are subject to the Mozilla Public License Version 1.1      }
 { (the "License"); you may not use this file except in compliance with the License.    }
@@ -14,7 +14,7 @@
 { The Original Code is CCR.ResaveCompleteDlg.pas.                                      }
 {                                                                                      }
 { The Initial Developer of the Original Code is Chris Rolliston. Portions created by   }
-{ Chris Rolliston are Copyright (C) 2009-2011 Chris Rolliston. All Rights Reserved.    }
+{ Chris Rolliston are Copyright (C) 2009-2012 Chris Rolliston. All Rights Reserved.    }
 {                                                                                      }
 {**************************************************************************************}
 
